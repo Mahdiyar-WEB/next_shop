4ca2c5fdb4d5b86992b833e4e359c95a26cc60f4
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-21 14:22:39 +0330

    develop CompleteProfileStep

---
+++ app/(auth)/login/_components/CompleteProfileStep.tsx
@@ -0,0 +1,118 @@
+import { Control, Controller, FieldErrors } from "react-hook-form";
+import { RegisterFormValues } from "./AuthForm";
+import { Box, Button, Typography } from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import Textfield from "components/Textfield";
+
+type UserInfoStepProps = {
+  control: Control<RegisterFormValues>;
+  errors: FieldErrors<RegisterFormValues>;
+  isSubmitting: boolean;
+};
+
+const CompleteProfileStep = ({
+  control,
+  errors,
+  isSubmitting,
+}: UserInfoStepProps) => {
+  return (
+    <Box>
+      <Typography variant="body1" className="font-bold!">
+        اطلاعات خود را تکمیل کنید
+      </Typography>
+
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        className="mb-6! mt-2!"
+      >
+        اطلاعات زیر برای ساخت حساب کاربری شما استفاده می‌شود.
+      </Typography>
+
+      <Box className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 mb-5">
+        <Controller
+          name="firstName"
+          control={control}
+          rules={{
+            required: "نام را وارد کنید.",
+            minLength: {
+              value: 2,
+              message: "نام حداقل ۲ کاراکتر باشد.",
+            },
+          }}
+          render={({ field }) => (
+            <Textfield
+              {...field}
+              autoFocus
+              fullWidth
+              label="نام"
+              error={!!errors.firstName}
+              helperText={errors.firstName?.message}
+            />
+          )}
+        />
+
+        <Controller
+          name="lastName"
+          control={control}
+          rules={{
+            required: "نام خانوادگی را وارد کنید.",
+            minLength: {
+              value: 2,
+              message: "نام خانوادگی حداقل ۲ کاراکتر باشد.",
+            },
+          }}
+          render={({ field }) => (
+            <Textfield
+              {...field}
+              fullWidth
+              label="نام خانوادگی"
+              error={!!errors.lastName}
+              helperText={errors.lastName?.message}
+            />
+          )}
+        />
+        <div className="sm:col-span-2">
+          <Controller
+            name="email"
+            control={control}
+            rules={{
+              required: "ایمیل را وارد کنید.",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "ایمیل معتبر نیست.",
+              },
+            }}
+            render={({ field }) => (
+              <Textfield
+                {...field}
+                dir="ltr"
+                type="email"
+                placeholder="test@gmail.com"
+                fullWidth
+                label="ایمیل"
+                icon={<EmailIcon />}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            )}
+          />
+        </div>
+      </Box>
+
+      <Box className="mt-5 shrink-0 border-t border-gray-200 pt-4">
+        <Button
+          fullWidth
+          type="submit"
+          variant="contained"
+          size="large"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? "در حال ثبت..." : "ثبت نام"}
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default CompleteProfileStep;
