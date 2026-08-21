95f1931183668c375abe4d735fcfadf6e3544fb1
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-21 16:32:56 +0330

    add type to completeProfile payloads

--- services/user-services.ts
+++ services/user-services.ts
@@ -1,12 +1,20 @@
 import { adminServices } from "./admin-services";
 import { apiClient } from "./client";
 
+type CompleteProfilePayload = {
+  name: string;
+  email: string;
+};
+
 export const userServices = {
   me: () => apiClient.get<{ user: unknown }>("/api/users/me"),
   updateMe: (data: unknown) =>
     apiClient.patch<{ user: unknown }>("/api/users/me", data),
-  completeProfile: (data: unknown) =>
-    apiClient.post<{ user: unknown }>("/api/users/complete-profile", data),
+  completeProfile: (data: CompleteProfilePayload) =>
+    apiClient.post<{ user: unknown; message: string }>(
+      "/api/users/complete-profile",
+      data,
+    ),
   getAll: (query = "") => adminServices.list<unknown>("users", query),
   update: (id: string, data: unknown) =>
     adminServices.update("users", id, data),
