b318c1c1763bd4ab6ddbcb324a1c706c8a15a23a
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-26 17:44:34 +0330

    develop middlewareAuth for proxy

---
+++ utils/middlewareAuth.ts
@@ -0,0 +1,51 @@
+import { NextRequest } from "next/server";
+
+export default async function middlewareAuth(req: NextRequest) {
+  try {
+    let accessToken = req.cookies.get("accessToken")?.value;
+    const refreshToken = req.cookies.get("refreshToken")?.value;
+
+    if (!accessToken && refreshToken) {
+      const refreshRes = await fetch(
+        `${process.env.BASE_URL}/api/auth/refresh`,
+        {
+          method: "POST",
+          headers: {
+            Cookie: `refreshToken=${refreshToken}`,
+          },
+        },
+      );
+
+      if (refreshRes.ok) {
+        const setCookie = refreshRes.headers.get("set-cookie");
+
+        const match = setCookie?.match(/accessToken=([^;]+)/);
+
+        accessToken = match?.[1];
+      }
+    }
+
+    if (!accessToken) {
+      console.log("null1");
+
+      return null;
+    }
+
+    const userRes = await fetch(`${process.env.BASE_URL}/api/users/me`, {
+      method: "GET",
+      headers: {
+        Cookie: `accessToken=${accessToken}`,
+      },
+    });
+
+    if (!userRes.ok) {
+      return null;
+    }
+
+    const result = await userRes.json();
+
+    return result?.data?.user ?? null;
+  } catch {
+    return null;
+  }
+}
