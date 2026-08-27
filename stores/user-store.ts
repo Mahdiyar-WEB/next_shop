a48a175a718aec3bc796cf263e7d274c52240b2a
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-27 09:51:52 +0330

    handle loading inside zustand for user-store

--- stores/user-store.ts
+++ stores/user-store.ts
@@ -3,12 +3,14 @@ import { create } from "zustand";
 
 interface UserStore {
   user: User | null;
+  isLoading: boolean;
   setUser: (user: User | null) => void;
   clearUser: () => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
   user: null,
-  setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null }),
+  isLoading: true,
+  setUser: (user) => set({ user, isLoading: false }),
+  clearUser: () => set({ user: null, isLoading: false }),
 }));
