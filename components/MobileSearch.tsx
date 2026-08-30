223ea27fa01e5e0068c76c5bac3bf9f4f0f5e210
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-31 00:52:53 +0330

    develop MobileSearch section

---
+++ components/MobileSearch.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+import SearchBox from "./SearchBox";
+
+const MobileSearch = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 1024);
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className="sticky bottom-6 w-fit mx-auto lg:hidden">
+      {isMobile && (
+        <SearchBox
+          className="flex w-72 rounded-xl px-3 shadow-lg border border-secondary-50 duration-150 focus-within:border-primary-500 focus-within:shadow-primary-100"
+          inputClassName="text-sm py-3"
+        />
+      )}
+    </div>
+  );
+};
+
+export default MobileSearch;
