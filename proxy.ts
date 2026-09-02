6a48bd28e72372b3494b05c311caf5c382c8db7b
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-09-03 02:10:18 +0330

    update proxy logic

--- proxy.ts
+++ proxy.ts
@@ -6,21 +6,15 @@ export async function proxy(request: NextRequest) {
   const user = await middlewareAuth(request);
 
   if (pathname === "/login" && user?.isActive) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (pathname.startsWith("/admin") && (!user || user.role !== "ADMIN")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  if (
-    pathname.startsWith("/admin") &&
-    (!user || user.role !== "ADMIN")
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  if (
-    pathname.startsWith("/dashboard") &&
-    (!user || user.role !== "USER")
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
+  if (pathname.startsWith("/dashboard") && (!user || user.role !== "USER")) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
