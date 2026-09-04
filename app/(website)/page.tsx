01d91d01e7fdeb72aa48be565ca7a6f653ead932
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-09-04 21:55:02 +0330

    develop main page and fetch products with Promise.all

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,9 +1,26 @@
 import { Metadata } from "next";
+import { productServices } from "services/product-services";
 
 export const metadata: Metadata = {
   title: "خانه",
 };
 
-export default function Home() {
+const fetchProducts = async () => {
+  try {
+    const [mobiles, laptops, accessories] = await Promise.all([
+      productServices.getAll("category=mobile&limit=10"),
+      productServices.getAll("category=laptop&limit=10"),
+      productServices.getAll("category=accessories&limit=10"),
+    ]);
+
+    return { mobiles, laptops, accessories };
+  } catch (error) {
+    throw new Error("خطا در بارگذاری اطلاعات");
+  }
+};
+
+export default async function Home() {
+  const { accessories, laptops, mobiles } = await fetchProducts();
+
   return <div className="h-[2000px] text-center">main</div>;
 }
