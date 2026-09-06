a1adc9b1287f70e18a60400f91ff2da7425f497b
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-09-06 07:59:50 +0330

    add remote url for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Enables Cache Components and Partial Prerendering in Next.js 16.
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pvrqyfd3icxrazgs.public.blob.vercel-storage.com",
+      },
+    ],
+  },
   cacheComponents: true,
 };
 
