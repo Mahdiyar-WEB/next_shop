d0558c4f834dc1082a5f9795757a57c3a2d697c0
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-09-10 06:26:18 +0330

    add ProductsSort into /categories/[categorySlug] route

--- app/(website)/categories/[categorySlug]/page.tsx
+++ app/(website)/categories/[categorySlug]/page.tsx
@@ -1,20 +1,30 @@
 import BreadCrumbs from "components/common/BreadCrumbs";
+import ProductsSort from "components/ui/ProductsSort";
 import ProductCard from "components/website/ProductCard";
 import { productServices } from "services/product-services";
+import queryString from "query-string";
 
 export const instant = false;
 
 const CategorySlug = async ({
   params,
+  searchParams,
 }: {
   params: Promise<{ categorySlug: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) => {
   const { categorySlug } = await params;
+  const searchOptions = await searchParams;
 
-  const { products } = await productServices.getAll(`category=${categorySlug}`);
+  const { products } = await productServices.getAll(
+    `category=${categorySlug}&${queryString.stringify(searchOptions)}`,
+  );
+  
   return (
     <main className="mx-auto mb-10 w-full overflow-hidden 2xl:max-w-screen-2xl">
       <BreadCrumbs />
+      {/* sort */}
+      <ProductsSort />
       {/* product container */}
       <div className="grid grid-cols-12">
         {products.map((product) => (
