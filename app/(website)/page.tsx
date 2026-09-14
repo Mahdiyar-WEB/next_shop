23393ac00c078aae8fe602e4dbeaeb922f2435b6
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-09-14 06:30:23 +0330

    add WebsiteBanner to main page

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import { productServices } from "services/product-services";
 import ProductsContainer from "components/website/ProductsContainer";
+import WebsiteBanner from "components/website/WebsiteBanner";
 
 export const metadata: Metadata = {
   title: "خانه",
@@ -25,15 +26,31 @@ export default async function Home() {
 
   return (
     <main className="mx-auto mb-10 w-full sm:w-[95%] 2xl:max-w-screen-2xl px-2 sm:px-0">
+      <WebsiteBanner
+        image="/mobile_banner.jpg"
+        alt="mobile_banner"
+        text="نسل جدید موبایل‌ها را کشف کنید"
+      />
       <ProductsContainer
         categoryPath="mobile"
         title="موبایل"
         products={mobiles.products}
       />
+      <WebsiteBanner
+        image="/laptop_banner.jpg"
+        alt="laptop_banner"
+        text="قدرت و سرعت را با لپ‌تاپ‌های جدید تجربه کنید"
+      />
+
       <ProductsContainer
         categoryPath="laptop"
         title="لپ تاپ"
         products={laptops.products}
+      />
+      <WebsiteBanner
+        image="/accessories_banner.jpg"
+        alt="accessories_banner"
+        text="اکسسوری‌های حرفه‌ای، برای تکمیل تجربه دیجیتال شما"
       />
       <ProductsContainer
         categoryPath="accessories"
