5ab7ecee65d588f60cb1a0d802c6b320d1596399
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-09-18 15:45:46 +0330

    develop Cart page

---
+++ app/(website)/cart/page.tsx
@@ -0,0 +1,11 @@
+import CartContainer from "./_components/CartContainer";
+
+const CartPage = () => {
+  return (
+    <main className="mx-auto w-full sm:w-[95%] 2xl:max-w-screen-2xl">
+      <CartContainer />
+    </main>
+  );
+};
+
+export default CartPage;
