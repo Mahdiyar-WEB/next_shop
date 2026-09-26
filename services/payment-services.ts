dd31a46e79c268cf925c8c9ad4f4c49f84d9f1da
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-09-26 19:03:09 +0330

    add getPayments to payments services

--- services/payment-services.ts
+++ services/payment-services.ts
@@ -1,9 +1,24 @@
+import { PaymentType } from "types/paymentType";
 import { apiClient } from "./client";
 import { AddressType } from "types/addressType";
+
+export interface PaymentsResponse {
+  payments: PaymentType[];
+  pagination: {
+    page: number;
+    limit: number;
+    totalItems: number;
+    totalPages: number;
+    hasNextPage: boolean;
+  };
+}
 
 export const paymentServices = {
   checkout: (address: AddressType) =>
     apiClient.post<{
       message: string;
     }>("/api/payments/checkout", address),
+
+  getPayments: (query = "") =>
+    apiClient.get<PaymentsResponse>(`/api/payments${query ? `?${query}` : ""}`),
 };
